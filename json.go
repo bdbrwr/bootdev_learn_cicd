3e4873046ddd65511d28c053519c46763002a48a
bdbrwr <bart@bdbrwr.nl>
2025-06-17 20:52:57 +0200

    CD

--- json.go
+++ json.go
@@ -30,8 +30,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err = w.Write(dat); err != nil {
-		log.Printf("Error writing response body: %s", err)
-		return
-	}
+	w.Write(dat)
 }
